47f056a743f6d2762f1dee6b3a728ee53204ddc5
Dhiraj Sharma <sharmadhiraj.np@gmail.com>
2024-04-12 15:59:27 +0545

    Remove duplicate nodes from an unsorted Linked List.

--- src/linked_list/remove_duplicates_from_unsorted_linked_list.dart
+++ src/linked_list/remove_duplicates_from_unsorted_linked_list.dart
@@ -2,19 +2,21 @@ import '../common/list_node.dart';
 
 void main() {
   final ListNode? head = ListNode.createLinkedList(
-    [1, 5, 7, 9, 9, 13, 15, 16, 16, 16, 22, 41, 41],
+    [45, 78, 2, 4, 7, 9, 76, 34, 41, 5, 12, 78, 4, 76, 2, 33],
   );
-  print(removeDuplicatesFromSortedLinkedList(head));
+  print(removeDuplicatesFromUnsortedLinkedList(head));
 }
 
-ListNode? removeDuplicatesFromSortedLinkedList(ListNode? head) {
+ListNode? removeDuplicatesFromUnsortedLinkedList(ListNode? head) {
+  final Set<int> nodes = {};
   final ListNode dummyHead = ListNode(val: 0);
   ListNode? current = dummyHead;
 
   while (head != null) {
-    if (current?.val != head.val) {
+    if (!nodes.contains(head.val)) {
       current?.next = head;
       current = current?.next;
+      nodes.add(head.val);
     }
     head = head.next;
   }
--- test/linked_list/remove_duplicates_from_unsorted_linked_list_test.dart
+++ test/linked_list/remove_duplicates_from_unsorted_linked_list_test.dart
@@ -1,37 +1,38 @@
 import 'package:test/test.dart';
 
 import '../../src/common/list_node.dart';
-import '../../src/linked_list/remove_duplicates_from_sorted_linked_list.dart';
+import '../../src/linked_list/remove_duplicates_from_unsorted_linked_list.dart';
 
 void main() {
   test(
     "Removing duplicates from a linked list",
     () {
       final ListNode? head1 = ListNode.createLinkedList(
-        [1, 5, 7, 9, 9, 13, 15, 16, 16, 16, 22, 41, 41],
+        [45, 78, 2, 4, 7, 9, 76, 34, 41, 5, 12, 78, 4, 76, 2, 33],
       );
-      final ListNode? result1 = removeDuplicatesFromSortedLinkedList(head1);
+      final ListNode? result1 = removeDuplicatesFromUnsortedLinkedList(head1);
       expect(
         result1,
-        equals(ListNode.createLinkedList([1, 5, 7, 9, 13, 15, 16, 22, 41])),
+        equals(ListNode.createLinkedList(
+            [45, 78, 2, 4, 7, 9, 76, 34, 41, 5, 12, 33])),
       );
 
       final ListNode? head2 = ListNode.createLinkedList(
         [1, 1, 1, 1, 1],
       );
-      final ListNode? result2 = removeDuplicatesFromSortedLinkedList(head2);
+      final ListNode? result2 = removeDuplicatesFromUnsortedLinkedList(head2);
       expect(result2, equals(ListNode.createLinkedList([1])));
 
       final ListNode? head3 = ListNode.createLinkedList(
         [1, 2, 3, 4, 5],
       );
-      final ListNode? result3 = removeDuplicatesFromSortedLinkedList(head3);
+      final ListNode? result3 = removeDuplicatesFromUnsortedLinkedList(head3);
       expect(result3, equals(ListNode.createLinkedList([1, 2, 3, 4, 5])));
 
       final ListNode? head4 = ListNode.createLinkedList(
         [],
       );
-      final ListNode? result4 = removeDuplicatesFromSortedLinkedList(head4);
+      final ListNode? result4 = removeDuplicatesFromUnsortedLinkedList(head4);
       expect(result4, equals(ListNode.createLinkedList([])));
     },
   );
